cec4fa164aaa3668a2c93e7c53615de7ed84fcee
Rukhsar Ahmed <rukhsarnaich0@gmail.com>
2024-09-02 18:26:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,184 @@
+///////////Chapter 09
+
+//Question no 01
+
+// var city= prompt("Enter the city name")
+
+// if (city =="karachi") {
+//     console.log("Welcome to city of Lights");
+// }
+
+//Question no 02
+
+// var gender= prompt("Please Input Your Gender")
+
+// if (gender == "male") {
+//     console.log("Good Morning Sir");
+// }
+// if (gender == "female") {
+//     console.log("Good Morning Ma'am");
+// }
+
+//Question no 03
+
+// var Signal_Color = prompt("Select the signal color RED,YELLOW,GREEN")
+
+// if (Signal_Color == "red") {
+//     console.log("MUST STOP");
+// }
+// if (Signal_Color == "yellow") {
+//     console.log("Ready to Move");
+// }
+// if (Signal_Color == "green") {
+//     console.log("Move Now");
+// }
+
+//Question no 04
+
+// var fuel = prompt ("input your remaining fuel")
+// if (fuel > 0.25) {
+//     console.log("Please Refill the fuel in your Car!");
+// }
+
+//Question no 05
+
+// var a = 4;
+// if (++a === 5){
+//     alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+//     alert("given condition for variable b is true");
+// }
+// else {
+//     alert("given condition for variable b is false");  
+// }
+
+// var c = 12;
+// if (c++ === 13){
+//     alert("condition 1 is true");
+// }
+// if (c === 13){
+//     alert("condition 2 is true");
+// }
+// if (++c < 14){
+//     alert("condition 3 is true");
+// }
+// if(c === 14){
+//     alert("condition 4 is true");
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+//     alert("The cost equals");
+// }
+
+// if (true){
+//     alert("True");
+// }
+// if (false){
+//     alert("False");
+// }
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
+
+
+//Question no 06
+
+// var totalMarks = 300;
+// var obtainedMarks = +prompt();
+// var result = (obtainedMarks/totalMarks)*100;
+
+// if (result >= 80){
+//     document.write("Grade:A-One Remarks:Excellent");
+// }
+// else if (result >= 70){
+//     document.write("Grade:A Remarks:Good");
+// }
+// else if (result >= 60){
+//     document.write("Grade:B Remarks:you need to improve");
+// }
+// else if (result < 60){
+//     document.write("Grade:fail Remarks:sorry");
+// }
+
+
+//Question no 07
+
+// var the_number = prompt("Dear User Guess the Secret Number Ranging from 1 to 10")
+// var Secret_Number = "5";
+// if (the_number === Secret_Number) {
+//     console.log("Bingo! Correct Answer");
+// }
+// if(the_number !== Secret_Number){
+//     console.log("Close enough to the correct answer");
+// }
+
+// Question no 8
+
+// var divisibleNum = prompt("Enter a Number");
+
+// if(divisibleNum / 3){
+//     console.log("The number is divisible by 3");
+// }
+
+//Question no 09
+
+// var getNum = +promt();
+// var obtainedMarks = +prompt();
+// var result =obtainedMarks % 2;
+
+// if( result == 0){
+//     alert ("The number is even.")
+// }
+// else{
+//     alert ("The number is odd.")
+// }
+
+// Question no 10
+
+// var temperature = prompt("Enter the Temperature. Acoording To Your Area.");
+
+// if(temperature > 40){
+//     alert("'It is too hot outside!'");
+// }
+// else if(temperature > 30){
+//     alert("The Weather is Normal.");
+// }
+// else if(temperature > 20){
+//     alert("Weather is cool.");
+// }
+// else if(temperature > 10){
+//     alert("OMG! weather is so Cool");
+// }
+
+//Question no 11 
+
+// var takeFirstNum = +prompt();
+// var takeExpression = prompt();
+// var takeSecondNum = +prompt();
+// var result;
+
+// if (takeExpression === "+"){
+//     result = takeFirstNum + takeSecondNum;
+//     document.write(result);
+// }
+// else if (takeExpression === "-"){
+//     result = takeFirstNum - takeSecondNum;
+//     document.write(result);
+// }
+// else if (takeExpression === "/"){
+//     result = takeFirstNum / takeSecondNum;
+//     document.write(result);
+// }
+
+// else if (takeExpression === "*"){
+//     result = takeFirstNum * takeSecondNum;
+//     document.write(result);
+// }
+// else{
+//     alert("you are wrong")
+// }
